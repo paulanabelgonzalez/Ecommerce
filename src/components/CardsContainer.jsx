ff6bc5bdb897528294d9dd455f7ba7ce5fcc134c
paula <gaby_pau@live.com.ar>
2024-08-15 12:57:05 -0300

    agregue carrousel

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,12 +1,15 @@
 import { Card } from "./Card";
 import { useState } from "react";
 import { Filters } from "./Filters";
+import { Carousel } from "./Carousel";
 
 export const CardsContainer = () => {
 	const [filter, setFilter] = useState("");
 	return (
 		<>
 			<Filters setFilter={setFilter} />
+
+			<Carousel />
 
 			<Card filter={filter} />
 		</>
