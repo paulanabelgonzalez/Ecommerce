01a065f04e2cff4d803ec42323332b63a9fef2fc
paula <gaby_pau@live.com.ar>
2024-09-26 18:36:11 -0300

    estile componente addButton

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -14,5 +14,29 @@ export const AddButton = () => {
 
 	const product = products.find((product) => product.id === id);
 
-	return <Button onClick={() => handleAdd(product)}>Añadir al carrito</Button>;
+	return (
+		<Button
+			onClick={() => handleAdd(product)}
+			sx={{
+				textDecoration: "none",
+				margin: "auto",
+				border: "3px solid #999999",
+				borderRadius: "23px",
+				color: "#525252",
+				textAlign: "center",
+				fontWeight: 600,
+				background: "linear-gradient(120deg, #5e5b5b, #f2f2f2, #5e5b5b)",
+				backgroundSize: "150% 150%",
+				transition: "background-position 0.5s ease",
+				"&:hover": {
+					backgroundPosition: "100% 0",
+				},
+				"&:active": {
+					boxShadow: "inset 0 4px 8px rgba(0, 0, 0, 0.5)",
+				},
+			}}
+		>
+			Añadir al carrito
+		</Button>
+	);
 };
